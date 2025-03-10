ba7880d331a589efa68020ac1ebf479cb5be9e12
israr ahemad khan <128677016+israrakhan@users.noreply.github.com>
2025-03-10 21:41:16 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,13 +17,13 @@ const Hero = () => {
         </p>
         <div className='mt-8 space-x-4'>
            
-           <a href="">
+           <a href="ik427094@gmail.com">
            <button
             className='bg-gradient-to-r from-green-400 to-blue-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button>
             </a> 
           
-           <a href="https://drive.google.com/file/d/1UqC29M25fjedqTu5WGTaNkkJeLuuHlki/view?usp=drive_link">
+           <a href="https://drive.google.com/file/d/1rmZFBzDM2eVAP_8Syh4KKmfqfyv_pWQt/view?usp=sharing">
            <button 
             className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>

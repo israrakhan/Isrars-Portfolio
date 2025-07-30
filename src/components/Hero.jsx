4f92e3c5433a5ccdb2acbddf6a85b0ffa5c07082
israr ahemad khan <128677016+israrakhan@users.noreply.github.com>
2025-07-30 14:13:00 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,7 +25,7 @@ cloud engineer with a strong foundation in Linux, AWS, and DevOps practices, ade
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Contact With Me</button>
             </a> 
           
-           <a href="https://drive.google.com/file/d/14L78rf0feF1ERA5GROebkRgQAKqt9iJl/view?usp=sharing"target="_blank" >
+           <a href="https://drive.google.com/file/d/10AuF9PqQvKhbadzF5obhFuKFYCEbTKUF/view?usp=drive_link"target="_blank" >
            <button 
             className='bg-gradient-to-r from-pink-500 to-yellow-500 text-white
             transform transition-transform duration-300 hover:scale-105 px-4 py-2 rounded-full'>Resume</button>

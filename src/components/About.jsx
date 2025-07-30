430d39fc86e744fb0249bacd561b8522a2bc0885
israr ahemad khan <128677016+israrakhan@users.noreply.github.com>
2025-07-30 14:21:48 +0530

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -92,7 +92,19 @@ const About = () => {
                   ></div>
                 </div>
               </div>
-
+              
+     <div className="flex items-center">
+                <label htmlFor="htmlandcss" className="w-2/12">
+                  Linux
+                </label>
+                <div className="grow bg-gray-800 rounded-full h-2.5">
+                  <div
+                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
+                    transform transition-transform duration-300 hover:scale-105 w-9/12"
+                  ></div>
+                </div>
+              </div>
+            </div>
               
               <div className="flex items-center">
                 <label htmlFor="htmlandcss" className="w-2/12">
@@ -108,18 +120,7 @@ const About = () => {
             </div>
           
               
-              <div className="flex items-center">
-                <label htmlFor="htmlandcss" className="w-2/12">
-                  Linux
-                </label>
-                <div className="grow bg-gray-800 rounded-full h-2.5">
-                  <div
-                    className="bg-gradient-to-r from-green-400 to-blue-500 h-2.5 rounded-full 
-                    transform transition-transform duration-300 hover:scale-105 w-9/12"
-                  ></div>
-                </div>
-              </div>
-            </div>
+         
               
               <div className="flex items-center">
                 <label htmlFor="htmlandcss" className="w-2/12">
